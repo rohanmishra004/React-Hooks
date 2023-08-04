bed97e49cb22bfceced73ffa86c8cb53e3bd99f9
Rohan Mishra <rohanmishra.004@gmail.com>
2023-08-04 17:41:59 +0530

    Example for react-useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+  //we have set the current state of the variable of count as 0
+  // setCount is the function that is use to update the current state
+  const [count, setCount]= useState(0)
+
+
+  //function to update the state
+  const decreamentCount = () => {
+    setCount(count-1)
+  }
+
+  const increamentCount = () => {
+    setCount(count+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={decreamentCount}>-</button>
+      <span>{count}</span>
+      <button onClick={increamentCount}>+</button>
     </div>
   );
 }
