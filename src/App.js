449052ab2317876d9d525aa4a912c9aec57a362f
Rohan Mishra <rohanmishra.004@gmail.com>
2023-08-07 16:33:18 +0530

    useState example elaborated

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React, {useState} from "react";
 
 
 function App() {
-  //we have set the current state of the variable of count as 0
-  // setCount is the function that is use to update the current state
-  const [count, setCount]= useState(0)
+  //here - 0 is the default state of count
+  //with useState when we are updating the state we are rerendering
 
+  //there are two ways to pass value to useState - one is by passing directly and other is by passing a function
+  // const [count , setCount] = useState(0), when we use it via function we ensure that the function runs only once
+  const [count, setCount] = useState(() => {
+    console.log('useState triggered')
+    //Default state is defined by declaring it along with return
+    return 0
+  })
 
-  //function to update the state
-  const decreamentCount = () => {
-    setCount(count-1)
+  const increament = () => {
+    setCount((prevCount)=>prevCount+1)
   }
 
-  const increamentCount = () => {
-    setCount(count+1)
+
+  const decrement = () => {
+    setCount((prevCount)=>prevCount-1)
   }
+
 
   return (
     <div className="App">
-      <button onClick={decreamentCount}>-</button>
+      <button onClick={decrement}>-</button>
       <span>{count}</span>
-      <button onClick={increamentCount}>+</button>
+      <button onClick={increament}>+</button>
     </div>
   );
 }
