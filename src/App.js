1e5317bd5f3281454eb27aa46f45dc3f86b4f453
Rohan Mishra <rohanmishra.004@gmail.com>
2023-08-07 17:44:51 +0530

    useEffect basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [resourceType , setResourceType]=useState('posts')
+
+
+
+  //the array at the end ensures that when we make the data in that particular data only then the useEffect change takes place and not on every single render
+  useEffect(() => {
+    console.log('render')
+    //This ensures that useEffect only runs when there is change in resoureceType
+  }, [resourceType])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={()=>setResourceType('posts')}>Posts</button>
+        <button onClick={()=>setResourceType('users')}>Users</button>
+        <button onClick={()=> setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
     </div>
   );
 }
